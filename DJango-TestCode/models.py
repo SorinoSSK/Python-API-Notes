c15914b1540eed47c79f1c8151d0f26045891dc3
SorinoSSK <sweeapplemage@gmail.com>
2023-07-04 17:34:10 +0800

    Push test code

--- DJango-TestCode/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-# Create your models here.
-
-# class testApi():
-#     def __init__(self, isSuccess):
-#         self.Success = isSuccess
--- DJango-TestCode/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-# from .models import testApi
-
-# class TestAPISerializer(serializers.Serializer):
-#     isSuccess = serializers.CharField()
